254ad470c7eb25d826af71ed043ffd725d0bfa8a
heitaailong <heitaailong.crowdworks@gmail.com>
2019-09-20 16:00:38 +0900

    commit all files

---
+++ sample.js
@@ -0,0 +1,18 @@
+/*
+
+$(function(){
+    $('#nav li').hover(
+        function(){
+            $(this).animate({padding: '10px 12px'}), function(){
+                $(this).css('background-color','rgba(116,156,158,1)');
+            });
+        },
+        function(){
+            $(this).animate({padding: '0px 12px'}, function(){
+                $(this).css('background-color','rgba(116,156,158,0.6)');
+            });
+        }
+    );
+});
+
+*/
